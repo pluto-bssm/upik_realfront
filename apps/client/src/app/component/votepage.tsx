417bf5efac0aa2622aadc57e0afa1d235684d332
김태현 <143389711+taehyun00@users.noreply.github.com>
2025-05-14 00:17:36 +0900

    feat(#4) :: 사이드바 완료 & 투표#2

--- apps/client/src/app/component/votepage.tsx
+++ apps/client/src/app/component/votepage.tsx
@@ -1,24 +1,29 @@
+import Votemenu from "../votemenu/page"
+
 export default function Votepage() {
 
-    const category="재미"
+    const category="재미질문"
     const title = "학교에서 가장 무서운것은?"
     const date="2025-05-23"
-    const IsEnd = false
+    const num = 2
 
     return (
       <div>
-        <div className="bg-[#FFFFFF] w-[120vh] h-[60vh] rounded-[2vh]">
-            <div>
-                {category}
-                <div>
-                    {title}
-                    <div>
-                        <p>{date}   투표마감</p>
+        <div className="bg-[#FAFAFA] w-[120vh] h-[60vh] rounded-[2vh]">
+            <div className="relative top-[4vh] left-[5vh]">
+                <p className="text-[#0158DE] font-['P_Regular'] text-[2vh]">{category}</p>
+                <div className="absolute top-[3vh]">
+                <p className="text-[#FFFFF] font-['P_Regular'] text-[3vh]">{title}</p>
+                    <div className="absolute top-[4vh]">
+                        <p className="text-[#A6A6A6] font-['P_Regular'] text-[1.5vh]">{date}   투표마감</p>
                     </div>
                 </div>
             </div>
         </div>
-  
+
+        <div className="absolute top-[45%] left-[5vh] h-[auto]">
+        <Votemenu/>
+        </div>
       </div>
     );
   }
