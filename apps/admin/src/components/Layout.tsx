f789bf5a709d277e415e5957a95f5de1d1b130b3
hwangjiyeong916 <bomaye119@gmail.com>
2025-06-24 01:06:50 +0900

    style :: logo 추가

---
+++ apps/admin/src/components/Layout.tsx
@@ -0,0 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import styled from "@emotion/styled";
+import color from "@/style/color";
+const Layout = () => {
+    return (
+        <StyledLayout>
+
+        </StyledLayout>
+    )
+}
+export default Layout;
+
+const StyledLayout = styled.div`
+    width: 242px;
+    height: 100vh;
+    background: ${color.gray800;
+`;
---
+++ apps/admin/src/style/color.ts
@@ -0,0 +1,13 @@
+const color = {
+    main: '#0158DE',
+    lightmain:'#80ABEF',
+    back_color:'#EFF5FF',
+    white: '#FFFFFF',
+    gray50: '#FAFAFA',
+    gray200: '#A6A6A6',
+    gray300: '#8B8B8B',
+    gray500: '#595959',
+    gray800: '#0D0D0D',
+};
+
+export default color;
